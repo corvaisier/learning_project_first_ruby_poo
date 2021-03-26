dda87a8e4f8b2f2820f3890abbdd02bd58180286
corvaisier <julien.corvaisier.br@gmail.com>
2021-03-26 11:03:52 +0100

    implement second test et second method of instance

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -2,4 +2,7 @@ class Greeting
     def hello
         "Hello world"
     end
+    def goodbye
+        "Goodbye world"
+    end
 end
--- spec/greetting_spec.rb
+++ spec/greetting_spec.rb
@@ -2,9 +2,16 @@ require "spec_helper"
 require "greeting"
 
 RSpec.describe Greeting do
-    it "return 'Hello world'" do
-        greeting = Greeting.new
-
-        expect(greeting.hello).to  eql("Hello world")
+    describe "#hello" do
+        it "return 'Hello world'" do
+            greeting = described_class.new
+            expect(greeting.hello).to  eql("Hello world")
+        end
+    end
+    describe "#goodbye" do 
+        it "return 'Goodbye world'" do
+            greeting = described_class.new
+            expect(greeting.goodbye).to  eql("Goodbye world")
+        end
     end
 end
