3ca18d9487ccae0e5cecda061ba8827bbed5a872
corvaisier <julien.corvaisier.br@gmail.com>
2021-03-29 09:34:26 +0200

    style

--- shop.rb
+++ shop.rb
@@ -5,12 +5,12 @@ require "./lib/client_interraction.rb"
 panier = Panier.new
 entre_client = EntreeClient.new
 reduction = Reduction.new
-client_interraction = ClientCnterraction.new
+client_interraction = ClientInterraction.new
 client_interraction.fruits
 ARGF.each do |argument|
-  for args in entre_client.entree(argument)
+  entre_client.entree(argument).each do |args|
     panier.add_fruits(args)
-    reduction.calcule_reduction(args, panier)
+    reduction.calcule_price_after_reduction(args, panier)
   end
   client_interraction.prix(panier)
 end
